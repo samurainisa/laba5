889f6b239ed8d53023f92c486f878d1bb77719c5
workspace <sadovikov.ne@kemerovorea.ru>
2021-11-29 21:23:39 +0700

    Добавьте файлы проекта.

---
+++ laba5/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace laba5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.textboxmin = new System.Windows.Forms.TextBox();
+            this.Ncolvo = new System.Windows.Forms.TextBox();
+            this.textboxmax = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(130, 223);
+            this.chart1.Name = "chart1";
+            series1.BorderColor = System.Drawing.Color.Transparent;
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series1.Color = System.Drawing.Color.Gainsboro;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.Color = System.Drawing.Color.Red;
+            series2.CustomProperties = "PixelPointWidth=1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series4";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Color = System.Drawing.Color.Red;
+            series4.Legend = "Legend1";
+            series4.Name = "Series5";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(954, 439);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(576, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Рассчёт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(573, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Результат";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(638, 155);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 13);
+            this.result.TabIndex = 3;
+            this.result.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textboxmin
+            // 
+            this.textboxmin.Location = new System.Drawing.Point(76, 43);
+            this.textboxmin.Name = "textboxmin";
+            this.textboxmin.Size = new System.Drawing.Size(100, 20);
+            this.textboxmin.TabIndex = 4;
+            this.textboxmin.Text = "0";
+            // 
+            // Ncolvo
+            // 
+            this.Ncolvo.Location = new System.Drawing.Point(76, 113);
+            this.Ncolvo.Name = "Ncolvo";
+            this.Ncolvo.Size = new System.Drawing.Size(100, 20);
+            this.Ncolvo.TabIndex = 5;
+            this.Ncolvo.Text = "100";
+            // 
+            // textboxmax
+            // 
+            this.textboxmax.Location = new System.Drawing.Point(76, 77);
+            this.textboxmax.Name = "textboxmax";
+            this.textboxmax.Size = new System.Drawing.Size(100, 20);
+            this.textboxmax.TabIndex = 6;
+            this.textboxmax.Text = "10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "N";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "(1/(1+exp(-1*(x-10))))+1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 723);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textboxmax);
+            this.Controls.Add(this.Ncolvo);
+            this.Controls.Add(this.textboxmin);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.TextBox textboxmin;
+        private System.Windows.Forms.TextBox Ncolvo;
+        private System.Windows.Forms.TextBox textboxmax;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ laba5/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MathNet.Symbolics;
+using Expr = MathNet.Symbolics.SymbolicExpression;
+
+namespace laba5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Expr expr;
+        public double eps;
+        public double h;
+        public double a;
+        public int N;
+        public double b;
+
+        public Func<double, double> f;
+
+        private void DrawPoint(double x)
+        {
+            chart1.Series[1].Points.AddXY(x, f(x));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            chart1.Series[3].Points.Clear();
+
+            a = Convert.ToDouble(textboxmin.Text);
+            b = Convert.ToDouble(textboxmax.Text);
+            N = Convert.ToInt32(Ncolvo.Text);
+            h = (b - a) / N;
+            
+            expr = Expr.Parse(textBox1.Text.ToString());
+            f = expr.Compile("x");
+            chart1.ChartAreas[0].AxisX.Minimum = 2 * a;
+            chart1.ChartAreas[0].AxisX.Maximum = 2 * b;
+            drawFunction();
+            result.Text = parabolasMethod().ToString();
+        }   
+
+
+        void drawFunction()
+        {
+            var h1 = (b - a) / 100;
+            for (double x = 1.5 * a; x <= 1.5 * (b + 0.001); x += h1)
+            {
+                var y = f(x);
+                chart1.Series[2].Points.AddXY(x, y);
+            }
+        }
+        double trapezesMethod()
+        {
+            double S = 0;
+            for (double x = a; x < (b + 0.01); x += h)
+            {
+                var y = f(x);
+                S += y;
+                chart1.Series[0].Points.AddXY(x, y);
+                chart1.Series[1].Points.AddXY(x, y);
+            }
+            var fa = f(a);
+            var fb = f(b);
+
+            S -= fa + fb; // not counting first and last one;
+            S = S * h + (h / 2) * (fa + fb);
+            return Math.Abs(S);
+        }
+
+        double rectanglesMethod()
+        {
+            double S = 0;
+            for (double x = a; x <= (b + 0.01); x += h)
+            {
+                var y = f(x);
+                S += y;
+                chart1.Series[0].Points.AddXY(x, y);
+                chart1.Series[0].Points.AddXY(x + h, y);
+                chart1.Series[1].Points.AddXY(x, y);
+            }
+            S = S * h;
+            return S;
+        }
+
+        double parabolasMethod()
+        {
+            double S = 0;
+            int count = 0;
+            int lastCount = (int)((b - a) / h);
+
+
+            for (double x = a; x <= (b + 0.01); x += h)
+            {
+                var y = f(x);
+
+                if (count == 0 || count == lastCount)
+                {
+                    S += y;
+                } else
+                {
+                    S += 2 * (count % 2 + 1) * y;
+                }
+                
+                ++count;                
+            }
+
+            for (double x = a; x <= (b + 0.01); x+= 2 * h)
+            {
+                var f1 = f(x);
+                var f2 = f(x + h);
+                var f3 = f(x + 2 * h);
+                var min = x - h;
+                var max = x + h;
+                var h2 = (max - min) / 10;
+                var funcA = (1.0 / 2) * (f1 - 2 * f2 + f3) / Math.Pow(h, 2);
+                var funcB = -(1.0 / 2) * (3 * f1 - 4 * f2 + f3) / h;
+                var funcC = f1;
+                var expression = Expr.Parse("a*x^2+b*x+c");
+                var func = expression.Compile("a", "b", "c", "x");
+                chart1.Series.Last().ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                for (double x2 = x; x2 <= x + 2 * h + 0.001; x2 += h2)
+                {
+                    var y = func(funcA, funcB, funcC, x2);                    
+                    chart1.Series[0].Points.AddXY(x2, y);
+                }
+            }            
+            S = S * h / 3;
+            return S;
+        }
+    }
+}
